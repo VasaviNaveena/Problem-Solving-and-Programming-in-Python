70aa5f823e081844bf75379a1f0fa345a0c2674d
VasaviNaveena Kunapareddi <45550992+VasaviNaveena@users.noreply.github.com>
2019-04-17 17:11:35 +0530

    Update unit_convert.py

--- Day1/unit_convert.py
+++ Day1/unit_convert.py
@@ -1,6 +1,6 @@
 #Take input values to convert 
 uc1 = input ("unit would you like to convert from: ")
-uc2 = input("Which unit would you like to convert to: ")
+uc2 = input("unit would you like to convert to: ")
 n = float(input ("Enter your value: " ))
 
 #Calculations
@@ -16,3 +16,4 @@ elif uc1 == "cm" and uc2 == "feet":
     print("%s feet" %(n/30.48))    
 elif uc1 == "feet" and uc2 == "cm":
     print("%s cm" %(n*30.48))    
+   
