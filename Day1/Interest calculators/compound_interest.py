f111eebc796e7e4e08e142daeeaa1aa2694feb86
VasaviNaveena Kunapareddi <45550992+VasaviNaveena@users.noreply.github.com>
2019-04-17 12:59:32 +0530

    Update compound_interest.py

--- Day1/Interest calculators/compound_interest.py
+++ Day1/Interest calculators/compound_interest.py
@@ -1,9 +1,12 @@
 print("COMPOUND INTEREST CALCULATION\n")
 print("Enter the Following Details:")
+
+#Take the values for principle Amount, Time,Rate of Interest
 PA = float(input("Enter Principle Amount: "))
 T = float(input("Enter the Time period in Years: "))
 R = float(input("Enter the Rate of Interest: "))
 
+# Calculates Compound interest  
 CI = PA * (pow((1 + R / 100), T)) 
 
 print("Compound interest is", CI) 
