7b62e99d5d9d9aaa8913d0d255d9abcb137840ea
VasaviNaveena Kunapareddi <45550992+VasaviNaveena@users.noreply.github.com>
2019-04-17 12:57:47 +0530

    Update simple_interest.py

--- Day1/Interest calculators/simple_interest.py
+++ Day1/Interest calculators/simple_interest.py
@@ -1,9 +1,12 @@
 print("SIMPLE INTEREST CALCULATION\n")
 print("Enter the Following Details:")
+
+#Take the values for principle Amount, Time,Rate of Interest
 PA = float(input("Enter Principle Amount: "))
 T = float(input("Enter the Time period in Years: "))
 R = float(input("Enter the Rate of Interest: "))
 
+# Calculates simple interest  
 SI = (PA * R * T) / 100
 
 print("simple interest is", SI) 
