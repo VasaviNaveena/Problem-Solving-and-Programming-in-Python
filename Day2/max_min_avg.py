b6dfcb5b5cc111537ae4b4d8f92e757314bb8c0e
VasaviNaveena Kunapareddi <45550992+VasaviNaveena@users.noreply.github.com>
2019-04-18 11:53:14 +0530

    Update max_min_avg.py

--- Day2/max_min_avg.py
+++ Day2/max_min_avg.py
@@ -1,7 +1,8 @@
 x = []
-for i in range(5):
-    n = int(input("Enter {0} number:".format(i+1)))
-    x.append(n)
+n = int(input("Enter how many numbers:"))
+for i in range(n):
+    a = int(input("Enter {0} number:".format(i+1)))
+    x.append(a)
 print("maximum number is:", max(x))
 print("minimum number is:", min(x))
 print("average number is:", sum(x)/len(x))
