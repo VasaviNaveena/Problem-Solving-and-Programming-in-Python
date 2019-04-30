b74bb15307646d9e5e2d8a23cc64efaf0eff0f2e
VasaviNaveena Kunapareddi <45550992+VasaviNaveena@users.noreply.github.com>
2019-04-30 15:32:02 +0530

    Update cos.py

--- cos.py
+++ cos.py
@@ -5,6 +5,7 @@ def cosine(x,n):
     for i in range(2, n, 2):
         pi=22/7
         y=x*(pi/180)
+        # cos expansion formula to find the value of every term  
         cosx = cosx + (sign*(y**i))/math.factorial(i)
         sign = -sign
     return cosx
